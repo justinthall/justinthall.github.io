ceb0294e01677d8f38fdcffab7c67e181f470e7d
justinthall <67346266+justinthall@users.noreply.github.com>
2026-04-27 23:02:37 +0000

    Deploying to gh-pages from @ justinthall/justinthall.github.io@ffe25e67de321eaa4ac12d77c910a1f2dd91bc69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,9 +437,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-thank-you-ma-39-am-unit-and-lesson",
+          title: 'Thank You Ma&amp;#39;am Unit and Lesson',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project%20copy/";
             },},{id: "teachings-data-science-fundamentals",
